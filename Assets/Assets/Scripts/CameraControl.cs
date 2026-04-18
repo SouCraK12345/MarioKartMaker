7b89f1a9f4a190c3be9b8f119bd5723c7317dde8
SouCraK12345 <181223239+SouCraK12345@users.noreply.github.com>
2026-04-18 20:30:27 +0900

    とりあえず走る

--- Assets/Assets/Scripts/CameraControl.cs
+++ Assets/Assets/Scripts/CameraControl.cs
@@ -1,9 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    private GameObject mainCamera;
-    public float angle;
+    public GameObject mainCamera;
+    private float angle_horizontal = 0;
+    private float angle_vertical = 0.4f;
+    private float distance = 5f; // カメラの距離
     private InputSystem_Actions inputActions;
 
     void Awake()
@@ -20,16 +23,18 @@ public class CameraControl : MonoBehaviour
     {
         inputActions.Disable();
     }
-    void Start()
-    {
-        mainCamera = GameObject.Find("Main Camera");
-    }
 
     // Update is called once per frame
     void Update()
     {
-        angle += inputActions.Player.Look.ReadValue<Vector2>().x / 50;
-        mainCamera.transform.localPosition = new Vector3(Mathf.Sin(angle) * 5, 1f, Mathf.Cos(angle) * 5);
+        Vector2 angle = inputActions.Player.Look.ReadValue<Vector2>();
+        angle_horizontal += angle.x / -50;
+        angle_vertical += angle.y / -50;
+        mainCamera.transform.position = new Vector3(
+            transform.position.x + Mathf.Cos(angle_horizontal) * Mathf.Cos(angle_vertical) * distance,
+            transform.position.y + Mathf.Sin(angle_vertical) * distance,
+            transform.position.z + Mathf.Sin(angle_horizontal) * Mathf.Cos(angle_vertical) * distance
+        );
         mainCamera.transform.LookAt(this.transform);
     }
 }
--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     private InputSystem_Actions inputActions;
-
+    private Rigidbody rb;
+    public GameObject mainCamera;
+    public float speed = 20f;
+    public float maxSpeed = 20f;
+    public float rotationSpeed = 2f;
     void Awake()
     {
         inputActions = new InputSystem_Actions();
@@ -21,10 +26,38 @@ public class PlayerController : MonoBehaviour
         inputActions.Disable();
     }
 
-    void Update()
+    void Start()
     {
-        // Vector2 move = inputActions.Player.Move.ReadValue<Vector2>();
-        // Debug.Log(move);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
         float accelerator = inputActions.Player.Accelerator.ReadValue<float>();
+        if (accelerator == 1f)
+        {
+            rb.AddForce(transform.forward * speed);
+            Vector3 forward = mainCamera.transform.forward;
+            forward.y = 0f;
+
+            if (forward.sqrMagnitude < 0.001f) return;
+
+            Quaternion targetRotation = Quaternion.LookRotation(forward);
+
+            // 補間（0〜1の割合）
+            Quaternion newRotation = Quaternion.Slerp(
+                rb.rotation,
+                targetRotation,
+                rotationSpeed * Time.fixedDeltaTime
+            );
+
+            rb.MoveRotation(newRotation);
+
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
     }
 }
